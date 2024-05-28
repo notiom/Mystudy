0e9d4c5d1f6d29b665cbdd7008221731bb1e6e63
notiom <99580052+notiom@users.noreply.github.com>
2024-05-28 18:32:47 +0800

    Update 19.cpp

--- leetcode/hot100/19.cpp
+++ leetcode/hot100/19.cpp
@@ -47,3 +47,61 @@ public:
 
 //2.官方题解
 ///////////////////////////////
+class Solution 
+{
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) 
+    {
+        vector<int> res;
+        if(matrix.empty()) return res;
+        int upper = 0;
+        int lower = matrix.size() - 1;
+        int left = 0;
+        int right = matrix.at(0).size() - 1;
+        while(true)
+        {
+            //从左到右遍历 - > 当i到有边界时直接退出本次循环
+            cout << "1: ";
+            for(int i = left;i <=right;i++)
+            {
+                res.push_back(matrix.at(upper).at(i));
+                cout << res.back();
+            }
+            cout << endl;        
+            //重新设定上边界，若上边界大于下边界，退出循环
+            if(++upper > lower) break; //避免了使用nextrow和nextcol等变量
+
+            //从上到下遍历 |||
+            cout << "2: ";
+            for(int i = upper;i <= lower;i++)
+            {
+                res.push_back(matrix.at(i).at(right));
+                cout << res.back();
+            }
+            cout << endl;
+            //重新设定右边界，若右边界小于左边界，退出循环
+            if(--right < left) break;
+            //从右到左遍历 <-(此处不能从right开始，不然右下角的数会遍历两次)
+            cout << "3: ";
+            for(int i = right;i >= left;i--)
+            {
+                res.push_back(matrix.at(lower).at(i));
+                cout << res.back();
+            }
+            cout << endl;
+            if(--lower < upper) break;
+
+            //从下到上遍历 ||
+            cout << "4: ";
+            for(int i = lower;i >= upper;i--)
+            {
+                res.push_back(matrix.at(i).at(left));
+                cout << res.back();
+            }
+            cout << endl;
+            if(++left > right) break;
+        }
+        return res;
+    }
+};
+//结论:通过，不需要设置方向数组，空间复杂度很低，关键是判断是否上下或者左右边界重合，且利用++提前判断下一个元素是否大于边界，如果大于，就退出
