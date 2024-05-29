a71ac54be77d5de2015aa006fb41fd39e655f1c5
notiom <99580052+notiom@users.noreply.github.com>
2024-05-29 18:51:49 +0800

    Update 23___.cpp

--- leetcode/hot100/23___.cpp
+++ leetcode/hot100/23___.cpp
@@ -1,1 +1,67 @@
-11
+//1.思路
+//保存当前遍历节点的前驱节点和后继节点，并将当前节点的下一个next指向pre
+class Solution 
+{
+public:
+    ListNode* reverseList(ListNode* head) 
+    {
+        //1 -> 2 -> 3 -> 4 -> 5
+        if(head == nullptr) return nullptr;
+        //思路,将原先的next指针的指向指向pre
+        ListNode* node = head;
+        //寻找下一个节点的下一个节点
+        ListNode* upper = node->next; //起始指向2
+        while(upper!=nullptr)
+        {
+            ListNode* pre = node;
+            if(pre == head)
+            {
+                pre->next = nullptr;
+            }
+            //寻找当前节点的下一个节点
+            node = upper;  //2
+            //寻找下一个节点的下一个节点
+            upper = node->next;
+            //现在开始重连指向
+            node->next = pre;//2->1
+            //但是node还是指向2此时
+        }
+        return node;
+        
+    }
+};
+//结论：通过，空间复杂度较低
+
+//2.官方题解
+//同上，我结合做了个小改动
+class Solution 
+{
+public:
+    ListNode* reverseList(ListNode* head) 
+    {
+        //1 -> 2 -> 3 -> 4 -> 5
+        if(head == nullptr) return nullptr;
+        //思路,将原先的next指针的指向指向pre
+        ListNode* node = head;
+        //前驱节点
+        ListNode* pre = head;
+        //寻找下一个节点的下一个节点
+        ListNode* upper = node->next; //起始指向2
+        pre->next = nullptr;
+        while(upper!=nullptr)
+        {
+            //寻找当前节点的下一个节点
+            node = upper;  //2
+            //寻找下一个节点的下一个节点
+            upper = node->next;
+            //现在开始重连指向
+            node->next = pre;//2->1
+            //但是node还是指向2此时
+            //初始化pre
+            pre = node;
+        }
+        return node;
+        
+    }
+};
+//结论：通过，时间降低
