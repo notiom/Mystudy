e5b4a767674c4fd6f9770fef71dbe86c68c7d80c
notiom <99580052+notiom@users.noreply.github.com>
2024-06-06 15:33:55 +0800

    Update 29____.cpp

--- leetcode/hot100/29____.cpp
+++ leetcode/hot100/29____.cpp
@@ -42,3 +42,52 @@ public:
         return head;
     }
 };
+//2.官方题解栈
+class Solution 
+{
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) 
+    {
+        //栈的思想
+        stack<ListNode*> stc;
+        ListNode* dummy = new ListNode(0,head);
+        ListNode* p = dummy;
+        while(p!=nullptr)
+        {
+            stc.push(p);
+            p = p->next;
+        }
+        //执行出栈n次，由先进后出知道第n个就是进去的倒数n
+        for(int i = 0; i < n; i++)
+        {
+            stc.pop();
+        }
+        //当前要删除节点的前驱节点
+        ListNode* pre = stc.top();
+        pre->next = pre->next->next;
+        return dummy->next;
+        //如果要删除头节点
+
+        return head;
+    }
+};
+
+//3.神之一手，递归
+class Solution 
+{
+    int count = 0;
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) 
+    {
+        //递归
+        if(head == nullptr)
+        {
+            return nullptr;
+        }
+        head->next = removeNthFromEnd(head->next,n);
+        count++;
+        return count==n ? head->next : head;
+    }
+};
+//结论：通过
+
