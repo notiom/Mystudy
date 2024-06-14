b4c22c88c23a5faa9db25455894a296dccbb889f
notiom <99580052+notiom@users.noreply.github.com>
2024-06-14 16:42:55 +0800

    Update 46_二叉树展开为链表.cpp

--- leetcode/hot100/46_二叉树展开为链表.cpp
+++ leetcode/hot100/46_二叉树展开为链表.cpp
@@ -47,3 +47,62 @@ public:
 private:
     queue<TreeNode*> res;
 };
+//结论：通过
+
+//2.官方题解
+//直接对二叉树进行变形
+class Solution 
+{
+public:
+    void flatten(TreeNode* root) 
+    {
+        TreeNode* node = root;
+        //当node指向null时说明所有交换已经完成
+        while(node != nullptr)
+        {
+            //当node左子树为空时，直接考虑下一个节点
+            if(node->left == nullptr) node = node->right;
+            else
+            {
+                //找到node左子树的最右节点
+                TreeNode* pre = node->left;
+                while(pre->right!= nullptr)
+                {
+                    pre = pre->right;
+                }
+                //将原来的右子树接到左子树的最右边节点
+                pre->right = node->right;
+                //将左子树插入到右子树的地方
+                node->right = node->left;
+                node->left = nullptr;
+                //考虑下一个节点
+                node = node->right;
+            }
+        }
+    }
+};
+//结论：很牛
+
+//3.官方题解解法2
+class Solution 
+{
+public:
+    void flatten(TreeNode* root) 
+    {
+        //使用先序遍历会造成当使1的right指向left时，right孩子丢失的问题
+        //但题主完美的利用后序遍历
+        //先遍历右孩子，这样之后在使其右跟节点指向的时候就不会出现孩子丢失的问题
+        //使用pre全局变量存放上一个指针
+
+        if(root == nullptr) return;
+        //遍历顺序为6 5 4 3 2 1 
+        flatten(root->right);
+        flatten(root->left);
+        root->right = pre;
+        root->left = nullptr;
+        pre = root;
+    }
+private:
+    TreeNode* pre = nullptr;
+};
+//使用后序遍历完美的解决了孩子丢失的问题
