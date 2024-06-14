2a3a2ee08abce2ce2b341a7ea84715a3a334bc23
notiom <99580052+notiom@users.noreply.github.com>
2024-06-14 15:43:29 +0800

    Update 45_二叉树的右视图.cpp

--- leetcode/hot100/45_二叉树的右视图.cpp
+++ leetcode/hot100/45_二叉树的右视图.cpp
@@ -48,3 +48,35 @@ public:
     }
 };
 //结论：通过
+
+
+//2.二叉树的题一定有递归的解法
+//官方题解
+class Solution 
+{
+public:
+    vector<int> rightSideView(TreeNode* root) 
+    {
+        dfs(root,0);
+        return res;
+    }
+    void dfs(TreeNode* root, int depth)
+    {
+        // 思路是先调用右子树，如果右子树不存在才会调用左子树，这时就需要设计一个size变量
+        // 用来记录是否他是最右边的元素，如果右边有元素，push进去之后当前容器元素个数等于深度
+
+        if(root == nullptr) return;
+        if(depth == res.size())
+        {
+            //说明首次遇到这个深度
+            res.push_back(root->val);
+        }
+        //先递归的调用右子树
+        dfs(root->right, depth + 1);
+        dfs(root->left, depth + 1);
+
+    }
+private:
+    vector<int> res;
+};
+//结论：通过
