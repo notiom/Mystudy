2130b5e0dd471eec49edd1aa15700376a5ceaa7a
notiom <99580052+notiom@users.noreply.github.com>
2024-06-19 15:21:01 +0800

    Create 54_图_ 实现前缀树.cpp

---
+++ leetcode/hot100/54_图_ 实现前缀树.cpp
@@ -0,0 +1,18 @@
+class Trie {
+public:
+    Trie() {
+
+    }
+    
+    void insert(string word) {
+
+    }
+    
+    bool search(string word) {
+
+    }
+    
+    bool startsWith(string prefix) {
+
+    }
+};
