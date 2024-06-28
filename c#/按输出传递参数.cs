1df11ec6d594fb337ccd6906c62f7d7040c0ea9b
notiom <99580052+notiom@users.noreply.github.com>
2024-06-28 17:58:39 +0800

    Update 按输出传递参数.cs

--- c#/按输出传递参数.cs
+++ c#/按输出传递参数.cs
@@ -1,29 +1,30 @@
+// 按输出传递参数，相当于用传入的参数接受在函数中改变的值
 using System;
 
 namespace CalculatorApplication
 {
    class NumberManipulator
    {
-      public void getValue(out int x )
+      public void getValues(out int x, out int y )
       {
-         int temp = 5;
-         x = temp;
+          Console.WriteLine("请输入第一个值： ");
+          x = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine("请输入第二个值： ");
+          y = Convert.ToInt32(Console.ReadLine());
       }
    
       static void Main(string[] args)
       {
          NumberManipulator n = new NumberManipulator();
          /* 局部变量定义 */
-         int a = 100;
-         
-         Console.WriteLine("在方法调用之前，a 的值： {0}", a);
+         int a , b;
          
          /* 调用函数来获取值 */
-         n.getValue(out a);
+         n.getValues(out a, out b);
 
          Console.WriteLine("在方法调用之后，a 的值： {0}", a);
+         Console.WriteLine("在方法调用之后，b 的值： {0}", b);
          Console.ReadLine();
-
       }
    }
 }
