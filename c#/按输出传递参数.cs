3889afa69e7c50bb52e28fcb498b3df8f5280dc1
notiom <99580052+notiom@users.noreply.github.com>
2024-06-28 17:56:48 +0800

    Create 按输出传递参数.cs

---
+++ c#/按输出传递参数.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace CalculatorApplication
+{
+   class NumberManipulator
+   {
+      public void getValue(out int x )
+      {
+         int temp = 5;
+         x = temp;
+      }
+   
+      static void Main(string[] args)
+      {
+         NumberManipulator n = new NumberManipulator();
+         /* 局部变量定义 */
+         int a = 100;
+         
+         Console.WriteLine("在方法调用之前，a 的值： {0}", a);
+         
+         /* 调用函数来获取值 */
+         n.getValue(out a);
+
+         Console.WriteLine("在方法调用之后，a 的值： {0}", a);
+         Console.ReadLine();
+
+      }
+   }
+}
