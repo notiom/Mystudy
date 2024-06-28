9a5d351a4ee4879cfe37cb77184f9cf998916593
notiom <99580052+notiom@users.noreply.github.com>
2024-06-28 11:27:37 +0800

    Update 66_二分查找_搜索旋转排序数组.cpp

--- leetcode/hot100/66_二分查找_搜索旋转排序数组.cpp
+++ leetcode/hot100/66_二分查找_搜索旋转排序数组.cpp
@@ -47,4 +47,34 @@ private:
         return nums.size() - 1;
     }
 };
-//结论：通过
+//结论：通过,利用官方分析时间复杂度是o(logn)
+
+// 2.官方题解
+class Solution 
+{
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        // 一定有一个区域是顺序区间
+        // 只要左边界小于右边界就可以
+        // 那么该数就一定在这个范围内
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) 
+        {
+            int mid = (left + right) >> 1;
+            if (nums[mid] == target) return mid;
+            if (nums[left] <= nums[mid]) 
+            {
+                // left 到 mid 是顺序区间
+                (target >= nums[left] && target < nums[mid]) ? right = mid - 1 : left = mid + 1;
+            }
+            else 
+            {
+                // mid 到 right 是顺序区间
+                (target > nums[mid] && target <= nums[right]) ? left = mid + 1 : right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+//结论：通过，先找到有序数组，如果该数在有序的区间内，那么就只要在这里找就可以，如果不在该区间内，那就在无需数组里找，下一轮循环仍然会有序
