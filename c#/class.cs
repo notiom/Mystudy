dbeaaad671716375301b0b013f37a8ad8ce743bd
notiom <99580052+notiom@users.noreply.github.com>
2024-06-29 11:02:37 +0800

    Create class.cs

---
+++ c#/class.cs
@@ -0,0 +1,65 @@
+// static关键字在类中的体现
+using System;
+namespace StaticVarApplication
+{
+    class StaticVar
+    {
+       public static int num;
+        public void count()
+        {
+            num++;
+        }
+        public int getNum()
+        {
+            return num;
+        }
+    }
+    class StaticTester
+    {
+        static void Main(string[] args)
+        {
+            StaticVar s1 = new StaticVar();
+            StaticVar s2 = new StaticVar();
+            s1.count();
+            s1.count();
+            s1.count();
+            s2.count();
+            s2.count();
+            s2.count();         
+            Console.WriteLine("s1 的变量 num： {0}", s1.getNum());
+            Console.WriteLine("s2 的变量 num： {0}", s2.getNum());
+            Console.ReadKey();
+        }
+    }
+}
+
+// 成员函数声明为 static，这样的函数只能访问静态变量。静态函数在对象被创建之前就已经存在。
+
+using System;
+namespace StaticVarApplication
+{
+    class StaticVar
+    {
+       public static int num;
+        public void count()
+        {
+            num++;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+    }
+    class StaticTester
+    {
+        static void Main(string[] args)
+        {
+            StaticVar s = new StaticVar();
+            s.count();
+            s.count();
+            s.count();                   
+            Console.WriteLine("变量 num： {0}", StaticVar.getNum());
+            Console.ReadKey();
+        }
+    }
+}
