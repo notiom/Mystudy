a77c265b6347d945d974a29f589d4199411c5892
notiom <99580052+notiom@users.noreply.github.com>
2024-07-03 11:04:06 +0800

    Create 异常.cs

---
+++ c#/异常.cs
@@ -0,0 +1,38 @@
+// 使用try执行语句
+// 使用catch捕获可能出错的情况
+// finally时一定会执行的语句
+// Throw用于抛出异常
+using System;
+namespace ErrorHandlingApplication
+{
+    class DivNumbers
+    {
+        int result;
+        DivNumbers()
+        {
+            result = 0;
+        }
+        public void division(int num1, int num2)
+        {
+            try
+            {
+                result = num1 / num2;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Exception caught: {0}", e);
+            }
+            finally
+            {
+                Console.WriteLine("Result: {0}", result);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            DivNumbers d = new DivNumbers();
+            d.division(25, 0);
+            Console.ReadKey();
+        }
+    }
+}
