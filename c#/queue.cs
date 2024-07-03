db38b97ef53a5b2c4f11fcefcf222dfc37983057
notiom <99580052+notiom@users.noreply.github.com>
2024-07-03 13:45:32 +0800

    Create queue.cs

---
+++ c#/queue.cs
@@ -0,0 +1,12 @@
+public virtual void Clear();
+从 Queue 中移除所有的元素。
+2	public virtual bool Contains( object obj );
+判断某个元素是否在 Queue 中。
+3	public virtual object Dequeue();
+移除并返回在 Queue 的开头的对象。
+4	public virtual void Enqueue( object obj );
+向 Queue 的末尾添加一个对象。
+5	public virtual object[] ToArray();
+复制 Queue 到一个新的数组中。
+6	public virtual void TrimToSize();
+设置容量为 Queue 中元素的实际个数。
