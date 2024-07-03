f73d153e28988b899954b9dcfc12f9c4b942005b
notiom <99580052+notiom@users.noreply.github.com>
2024-07-03 11:30:24 +0800

    Create Hastable.cs

---
+++ c#/Hastable.cs
@@ -0,0 +1,10 @@
+1	public virtual void Add( object key, object value );
+向 Hashtable 添加一个带有指定的键和值的元素。
+2	public virtual void Clear();
+从 Hashtable 中移除所有的元素。
+3	public virtual bool ContainsKey( object key );
+判断 Hashtable 是否包含指定的键。
+4	public virtual bool ContainsValue( object value );
+判断 Hashtable 是否包含指定的值。
+5	public virtual void Remove( object key );
+从 Hashtable 中移除带有指定的键的元素。
