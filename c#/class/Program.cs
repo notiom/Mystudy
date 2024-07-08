12b151d8b153f7686cfbc197d070cda00eca749d
notiom <99580052+notiom@users.noreply.github.com>
2024-07-08 19:44:56 +0800

    Update Program.cs

--- c#/class/Program.cs
+++ c#/class/Program.cs
@@ -6,7 +6,6 @@ namespace Drived
     {
         static void Main(string[] args)
         {
-            // 类的虚函数等等
             Boss boss1 = new Boss(100,10000,20);
             boss1.Print();
 
@@ -14,6 +13,7 @@ namespace Drived
             Enemy enemy = new Boss(100,10000,30); //本质上是子类对象
             // enemy.Skill(); //无法调用，因为变量还是Enemy对象
             enemy.Move(); //因为是虚方法，所以在调用时会调用子类的方法
+            enemy.Notice(); //隐藏方法会调用父类的Notice
         }
     }
 }
