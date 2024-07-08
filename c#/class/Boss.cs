305b30a4700b6e7d10ea0fecdc7c297452f29114
notiom <99580052+notiom@users.noreply.github.com>
2024-07-08 19:40:19 +0800

    Create Boss.cs

---
+++ c#/class/Boss.cs
@@ -0,0 +1,31 @@
+using System;
+namespace Drived
+{
+    class Boss : Enemy
+    {
+        private int attack;
+        public Boss(int attack,int hp,int speed)
+        {
+            this.attack = attack;
+            this.hp = hp;
+            this.speed = speed;
+        }
+        public void Skill()
+        {
+            Console.WriteLine("Boss Skill");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("HP : " + hp);
+            Console.WriteLine("Speed : " + speed);
+            
+        }
+
+        public override void Move()
+        {
+            // 重写
+            Console.WriteLine("Boss Move");
+        }
+    }
+}
