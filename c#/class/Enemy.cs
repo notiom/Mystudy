683f6891c66c97096d85c208e9bbf24a31b770e9
notiom <99580052+notiom@users.noreply.github.com>
2024-07-08 19:40:38 +0800

    Create Enemy.cs

---
+++ c#/class/Enemy.cs
@@ -0,0 +1,19 @@
+using System;
+namespace Drived
+{
+    class Enemy
+    {
+        protected int hp;
+        protected int speed;
+
+        public void AI()
+        {
+            Console.WriteLine("AI");
+        }
+
+        public virtual void Move()
+        {
+            Console.WriteLine("Move");
+        }
+    }
+}
