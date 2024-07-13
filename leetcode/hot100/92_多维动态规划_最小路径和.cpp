b5ee44977599c8267ad737a9fbf8b80d2a66c442
notiom <99580052+notiom@users.noreply.github.com>
2024-07-13 15:36:13 +0800

    Update 92_多维动态规划_最小路径和.cpp

--- leetcode/hot100/92_多维动态规划_最小路径和.cpp
+++ leetcode/hot100/92_多维动态规划_最小路径和.cpp
@@ -36,3 +36,36 @@ public:
 };
 // 结论:通过 时间复杂度 o(m * n) 空间复杂度o(n)
 
+// 2.不消耗额外空间的改进方案
+// 直接改变原数组
+class Solution 
+{
+public:
+    int minPathSum(vector<vector<int>>& grid) 
+    {
+        // 最短路径是上一个位置的上面的加左边的得到的最小值作为当前值
+
+        // 先初始化第一行和第一列
+        // 优化方式为直接使用原矩阵更新，不使用额外的空间
+        int m = grid.size();
+        int n = grid.at(0).size();
+        for(int i = 1;i < n;i++)
+        {
+            // 初始化第一行
+            grid.at(0).at(i) = grid.at(0).at(i - 1) + grid.at(0).at(i);
+        }
+
+        for(int j = 1;j < m;j++)
+        {
+            grid.at(j).at(0) = grid.at(j - 1).at(0) + grid.at(j).at(0);
+            for(int k = 1;k < n;k++)
+            {
+                // 两层循环计算动态规划方程
+                // res的当前值应该为上方的元素的最小值加上左边加当前元素的最小值
+                grid.at(j).at(k) = min((grid.at(j - 1).at(k) + grid.at(j).at(k)),grid.at(j).at(k - 1) + grid.at(j).at(k));
+            }
+        }
+        return grid.at(m - 1).at(n - 1);
+    }
+};
+// 结论:空间复杂度o(1).
