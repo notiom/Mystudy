384be99ab9dcdd4f9ed42ba3c5d1fad488ac42a7
notiom <99580052+notiom@users.noreply.github.com>
2024-07-16 09:46:55 +0800

    Update 93_多维动态规划_最长回文子串.cpp

--- leetcode/hot100/93_多维动态规划_最长回文子串.cpp
+++ leetcode/hot100/93_多维动态规划_最长回文子串.cpp
@@ -42,3 +42,36 @@ public:
 };
 
 // 结论:通过 时间复杂度 o(n * n * n) 空间复杂度 o(1).
+
+// 2.官方题解动态规划
+class Solution 
+{
+public:
+    string longestPalindrome(string s) 
+    {
+        // 使用动态规划
+        // 使用倒序遍历的动态规划
+        int n = s.length();
+        string res = "";
+        vector<vector<bool>> dp(n,vector<bool>(n,false));
+        for(int i = n - 1;i >= 0;i--)
+        {
+            for(int j = i;j < n;j++)
+            {
+                // 使用正序排列会出现前面用到后面的数据但并未被初始化
+                if(s.at(i) == s.at(j) && (j - i <= 1 || dp.at(i + 1).at(j - 1)))
+                {
+                    // 当只有一个数或者两个数时并且这两个数相同,dp赋值为true
+                    dp.at(i).at(j) = true;
+                    if(res.size() < j - i + 1)
+                    {
+                        res = s.substr(i, j - i + 1);
+                    }
+                }
+            }
+        }
+        return res;
+
+    }
+};
+// 结论:通过 时间复杂度和空间复杂度都为o(n * n).
