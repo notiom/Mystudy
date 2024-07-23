b6ada0daed9996ccbd9fb2484c905102c96753d4
notiom <99580052+notiom@users.noreply.github.com>
2024-07-23 21:25:27 +0800

    Update 83_动态规划_打家劫舍______________.cpp

--- leetcode/hot100/83_动态规划_打家劫舍______________.cpp
+++ leetcode/hot100/83_动态规划_打家劫舍______________.cpp
@@ -26,3 +26,38 @@ public:
 //2.空间优化
 
 //3.官方题解
+
+
+// 4.我自己的解
+// 打家劫舍2
+class Solution 
+{
+public:
+    int rob(vector<int>& nums) 
+    {
+        int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        vector<int> res1(n + 1,0);
+        vector<int> res2(n + 1,0);
+        // 动态规划方程
+        // res1[i] = nums[i - 1] + res2[i - 1];
+        // res2[i] = max(res1[i - 1], res2[i - 1])
+        for(int i = 2; i <= n; i++)
+        {
+            res1[i] = nums[i - 1] + res2[i - 1];
+            res2[i] = max(res1[i - 1], res2[i - 1]);
+        }
+        int res = max(res1[n], res2[n]);
+
+        res1.clear();
+        res2.clear();
+        for(int i = 1; i < n; i++)
+        {
+            res1[i] = nums[i - 1] + res2[i - 1];
+            res2[i] = max(res1[i - 1], res2[i - 1]);
+        }
+
+        return max(res,max(res1[n - 1],res2[n - 1]));
+    }
+};
