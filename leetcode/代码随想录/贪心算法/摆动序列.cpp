0f69c697000e55bb051bc31b16c872e449c953b9
notiom <99580052+notiom@users.noreply.github.com>
2024-09-29 09:04:36 +0800

    Update 摆动序列.cpp

--- leetcode/代码随想录/贪心算法/摆动序列.cpp
+++ leetcode/代码随想录/贪心算法/摆动序列.cpp
@@ -32,3 +32,36 @@ public:
     }
 };
 // 通过 时间复杂度o(n),空间复杂度o(1).
+
+// 2.动态规划
+// 选取一个波峰和波谷
+class Solution 
+{
+public:
+    int wiggleMaxLength(vector<int>& nums) 
+    {
+        // 和分发饼干稍微有点类似,贪心的本质就是从第一个不满足的地方开始重新计数,如果有大于前面res的,就替换
+        int n = nums.size();
+        // 使用动态规划
+        // dp[i][0]表示当前作为波峰的最大值
+        // dp[i][1]表示当前作为波谷的最大值
+        vector<std::pair<int,int>> dp(n);
+        dp[0].first = 1;
+        dp[0].second = 1;
+        for(int i = 1;i < n;i++)
+        {
+            dp[i].first = dp[i].second = 1; // 先将1赋值
+
+            for(int j = 0;j < i;j++)
+            {
+                // 将波峰赋值,为上一次迭代的最大值或者当前元素为波谷时的最大值
+                if(nums[j] < nums[i]) dp[i].first = max(dp[i].first,dp[j].second + 1);
+                // 将波谷赋值,为上一次迭代的最大值或者当前元素为波峰时的最大值
+                if(nums[j] > nums[i]) dp[i].second = max(dp[i].second,dp[j].first + 1);
+            }
+
+        }
+        return max(dp[n - 1].first,dp[n - 1].second);
+    }
+};
+// 结论:通过 时间复杂度o(n  * n),空间复杂度o(n).
