7acb0cc6317e72b9113343d0afb273d1d7e34d15
notiom <99580052+notiom@users.noreply.github.com>
2024-10-11 08:28:45 +0800

    Update 最长公共子序列.cs

--- leetcode/代码随想录/动态规划/最长公共子序列.cs
+++ leetcode/代码随想录/动态规划/最长公共子序列.cs
@@ -26,4 +26,33 @@ public class Solution
 }
 // 结论: 通过 时间复杂度和空间复杂度都为o(m  * n).
 
-// 2.
+// 2. 空间复杂度优化
+public class Solution
+{
+    public int LongestCommonSubsequence(string text1, string text2) 
+    {
+        // 使用动态规划
+        // 使用二维dp数组
+        int m = text1.Length;
+        int n = text2.Length;
+
+        int [] dp1 = new int[n + 1];
+        int [] dp2 = new int[n + 1];
+
+        for(int i = 0;i < m;i++)
+        {
+            for(int j = 0;j < n;j++)
+            {
+                if(text1[i] == text2[j])
+                    dp2[j + 1] = dp1[j] + 1;
+                else
+                    dp2[j + 1] = Math.Max(dp1[j + 1],dp2[j]);
+                
+            }
+            // 该处需要copy
+            Array.Copy(dp2, dp1, dp2.Length);
+        }
+        return dp2[n];
+    }
+}
+// 空间复杂度为o(n).
